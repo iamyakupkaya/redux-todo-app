be9457fc13681f316228baf7fb895a7ff4a6f257
Yakup KAYA <iamyakupkaya@outlook.com>
2022-03-26 01:18:44 +0300

    update method was added

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { connect } from "react-redux";
-import { addTodos, removeTodos } from "../redux/todoSlice";
+import { addTodos, removeTodos, updateTodos } from "../redux/todoSlice";
 import { v4 as uuidv4 } from "uuid"; // uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 //following function transform State into Props.
@@ -24,35 +24,38 @@ const mapDispatchToProps = (dispatch) => {
     addTodo: (obj) => dispatch(addTodos(obj)),
     //removing method clone from original removeTodos from todoSlice.js
     removeTodo: (ID) => dispatch(removeTodos(ID)),
+    //updating method clone from original updateTodos from todoSlice.js
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
   };
 };
 
 const Todos = (props) => {
-  console.log("Props:", props);
 
   /*
    ********************* DESTRUCTURING AREA *********************
    */
-  const { todos, addTodo, removeTodo } = props; // props is a object {todos: Array(0), addTodo: ƒ}
+  const { todos, addTodo, removeTodo, updateTodo } = props; // props is a object {todos: Array(0), addTodo: ƒ}
 
   /*
    ********************* USING HOOKS AREA *********************
    */
   const [todo, setTodo] = useState("");
 
+  const inputRef = useRef(true);
+  console.log("gelen inputRef:", inputRef);
+
   useEffect(() => {
     // this is for initial value (todo) again and in input value will have been empty after click button
     setTodo("");
   }, [todos]); //just todos change invoked this useEffect.
 
-  console.log("todos", todos);
 
   /*
    ********************* CUSTOM FUNCTIONS AREA *********************
    */
 
   //following method for input area
-  const handleClick = (event) => {
+  const addClick = (event) => {
     addTodo({
       id: uuidv4(),
       item: todo, // todo is value of input area from user
@@ -65,8 +68,20 @@ const Todos = (props) => {
   };
   //following method for button of REMOVE
   const removeClick = (ID) => {
-    console.log("gelen id:", ID);
     removeTodo(ID);
+  };
+  //updating method for button of UPDATE
+  const handleUpdate = (id, value, event) => {
+    console.log("glen event.wvhich:", event.which);
+    if(event.which===13){ //13 number for enter. when user press enter event.which turns 13 number.
+      updateTodo({id, item:value});
+      inputRef.current.disabled=true;
+    }
+    //updateTodo(obj);
+  };
+  const changeInputRef = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
   };
 
   return (
@@ -77,7 +92,7 @@ const Todos = (props) => {
         className="todo-input"
         value={todo}
       />
-      <button className="add-btn" onClick={(e) => handleClick(e)}>
+      <button className="add-btn" onClick={(e) => addClick(e)}>
         ADD
       </button>
       <br></br>
@@ -87,7 +102,13 @@ const Todos = (props) => {
           {todos.map((todo) => {
             return (
               <li key={todo.id}>
-                {todo.item}
+                <textarea
+                  ref={inputRef}
+                  disabled={inputRef.current}
+                  defaultValue={todo.item}
+                  onKeyPress={(e)=>handleUpdate(todo.id,inputRef.current.value,e)}
+                />
+                <button onClick={() => changeInputRef()}>EDIT</button>
                 <button onClick={() => removeClick(todo.id)}>DELETE</button>
               </li>
             );
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,27 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState=[];
+const initialState = [
+];
 
 const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    // if we don't use reduxjs/toolkit we must be careful aboyt state...
+    // when we fill it. we should be carefull about...
+    // mutable and immutable types.!
 
-    name:"todos",
-    initialState,
-    reducers:{
-// if we don't use reduxjs/toolkit we must be careful aboyt state...
-// when we fill it. we should be carefull about...
-// mutable and immutable types.!
-
-        //adding method for todos
-        addTodos: (state, action) => {
-            state.push(action.payload);
-            return state;
-        },
-        //removing method for Todos
-        removeTodos: (state, action) => {
-            return state.filter(element => element.id !== action.payload); //action.payload will be just a id not an object.
-        }
-    } 
+    //adding method for todos
+    addTodos: (state, action) => {
+      state.push(action.payload);
+      return state;
+    },
+    //removing method for Todos
+    removeTodos: (state, action) => {
+      return state.filter((element) => element.id !== action.payload); //action.payload will be just a id not an object.
+    },
+    //updating method for todos
+    updateTodos: (state, action) => {
+      return state.map((todo) => {
+        /* //if we dont use ternary (condition ? true : false) we should use if-else structure
+                if (todo.id === action.payload.id) {
+                    return {
+                        ...todo,
+                        item: action.payload.item,
+                        completed: action.payload.completed,
+                    }
+                }
+                return todo;
+                totaly same with following code
+                */
+        return todo.id === action.payload.id
+          ? {
+              ...todo,
+              item: action.payload.item,
+              //completed: action.payload.completed,
+            }
+          : todo;
+      });
+    },
+  },
 });
 
 export const reducerTodo = todoSlice.reducer;
-export const {addTodos, removeTodos} = todoSlice.actions;
+export const { addTodos, removeTodos, updateTodos } = todoSlice.actions;
